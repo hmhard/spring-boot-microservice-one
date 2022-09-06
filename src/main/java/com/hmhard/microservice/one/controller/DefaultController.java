90068b7afe4f2b9b3805cda66347237e2cbf4333
Miniyahil Kebede <mkebede@jsi.local>
2022-09-06 11:40:47 +0300

    initial commit

---
+++ src/main/java/com/hmhard/microservice/one/controller/DefaultController.java
@@ -0,0 +1,14 @@
+package com.hmhard.microservice.one.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class DefaultController {
+
+@GetMapping("/")
+    public String index() throws Exception {
+    return "Welcome to microservice controller";
+}
+}
---
+++ src/test/java/com/hmhard/microservice/one/MicroserviceOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hmhard.microservice.one;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
